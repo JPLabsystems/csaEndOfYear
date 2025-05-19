18d4e566b0d7e864ec6fdc9abd741e9c38e3028b
Justinas Petkauskas <jplabsystems@gmail.com>
2025-05-19 09:18:19 -0400

    added Card constructor uwu

--- src/Card.java
+++ src/Card.java
@@ -1,2 +1,34 @@
-public class Card {
+public class Card
+{
+    private int value;
+    private int faceValue
+    private String name;
+
+    /*
+    values:
+    0: spade
+    1: clubs
+    2: diamond
+    3: hearts
+     */
+
+    public Card(int v, int n)
+    {
+        value = v;
+        faceValue = n;
+        switch (n) {
+            case 0:
+                name = "spades";
+                break;
+            case 1:
+                name = "clubs";
+                break;
+            case 2:
+                name = "diamonds";
+                break;
+            case 3:
+                name = "hearts";
+                break;
+        }
+    }
 }
