daba9881e79c141872736f29598f08a9e57a22be
Justinas Petkauskas <jplabsystems@gmail.com>
2025-05-20 09:34:39 -0400

    edited main

--- src/main.java
+++ src/main.java
@@ -3,9 +3,11 @@ import java.util.*;
 public class main {
     public static void main(String[] args)
     {
+        Scanner in = new Scanner(System.in);
+
         System.out.println("Hello World!");
         System.out.println("JAVA END OF YEAR!");
-        Card testCard = new Card(0, 0);
+
         Deck testDeck = new Deck();
     }
 }
