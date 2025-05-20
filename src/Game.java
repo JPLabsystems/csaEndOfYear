d1535ba5899f0d4ea09027f162683d393f5b63b5
dqbomb <alex_yu26@collegiate-va.org>
2025-05-20 09:37:54 -0400

    making game class

--- src/Game.java
+++ src/Game.java
@@ -1,2 +1,15 @@
 public class Game {
+
+    private int numPlayers;
+    
+
+    public Game(int num) {
+        numPlayers = num;
+    }
+
+    public void StartGame() {
+        // reset the deck
+        // Initiate the number of players
+
+    }
 }
