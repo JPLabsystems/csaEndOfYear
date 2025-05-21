59db5f1c1f9ad862474470117c5760feccbf2d1f
Justinas Petkauskas <jplabsystems@gmail.com>
2025-05-21 08:42:20 -0400

    NUMPLAYERS

--- src/main.java
+++ src/main.java
@@ -1,12 +1,21 @@
 import java.util.*;
 
+
 public class main {
+
     public static void main(String[] args)
     {
+        int numPlayers;
+
         Scanner in = new Scanner(System.in);
 
-//        System.out.println("Hello World!");
         System.out.println("JAVA END OF YEAR!");
+        System.out.print("WELCOME TO BLACKJACK. PLEASE ENTER THE NUMBER OF PLAYERS: ");
+
+        numPlayers = in.nextInt();
+
+        Game testGame = new Game(numPlayers);
+
 
 
     }
